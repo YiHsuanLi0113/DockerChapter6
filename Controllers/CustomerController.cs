736c4e3649919c38626c23670dd3f3ee97aecdf0
ysl19950113 <ysl19950113@gmail.com>
2024-03-15 12:03:06 +0800

    6_4_Repository_Route_更改CustomersController, 建構注入DAO  Repository及IConfigration(取出appsettings.json設定檔)，並加入自訂路由及路由條件約束。

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,3 +1,4 @@
+using ManyToManyCodeFirst.Data;
 using ManyToManyCodeFirst.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,63 +10,74 @@ namespace ManyToManyCodeFirst.Controllers;
 [ApiController]
 public class CustomersController : ControllerBase
 {
-    //using ManyToManyCodeFirst.Models;  
-    private readonly OrderContext _context;
-    public CustomersController(OrderContext context)
+    private readonly ICustomerRepo _repository;
+    private readonly IConfiguration _config;
+    public CustomersController(ICustomerRepo repository, IConfiguration config)
     {
-        _context = context;
+        _repository = repository;
+        _config = config;
     }
 
 
     // GET: api/Customers
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+    public ActionResult<IEnumerable<Customer>> GetCustomers()
     {
-        if (_context.Customers == null)
+        if (!_repository.GetAllCustomers().Any())
         {
             return NotFound();
         }
-        //using Microsoft.EntityFrameworkCore;
-        return await _context.Customers.ToListAsync();
+        var customers = _repository.GetAllCustomers();
+        foreach (Customer item in customers)
+        {
+        string pattern = @"\d{5}$";
+        string replacement = "xxxxx";
+        string input = item.SecurityID;
+        //using System.Text.RegularExpressions;
+        item.SecurityID=Regex.Replace(input, pattern, replacement);
+        }
+        return Ok(customers);
     }
 
 
     // GET: api/Customers/5
     [HttpGet("{id}")]
-    public async Task<ActionResult<Customer>> GetCustomer(int id)
+    public ActionResult<Customer> GetCustomer(int id)
     {
-        if (_context.Customers == null)
+        if (!_repository.GetAllCustomers().Any())
         {
             return NotFound();
         }
-       
-        var customer = await _context.Customers.FindAsync(id);
+        var customer = _repository.GetCustomerByID(id);
+
+
         if (customer == null)
         {
             return NotFound();
         }
-        return customer;
+        return Ok(customer);
     }
-   
+
+
     // PUT: api/Customers/5
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPut("{id}")]
-    public async Task<IActionResult> PutCustomer(int id, Customer customer)
+    public IActionResult PutCustomer(int id, Customer customer)
     {
         if (id != customer.CustomerId)
         {
             return BadRequest();
         }
+        _repository.UpdateCustomer(customer);  
 
 
-        _context.Customers.Update(customer);
         try
         {
-            await _context.SaveChangesAsync();
+            _repository.SaveChanges();  
         }
         catch (DbUpdateConcurrencyException)
         {
-            if (!CustomerExists(id))
+            if (_repository.GetCustomerByID(id)==null)
             {
                 return NotFound();
             }
@@ -81,46 +93,125 @@ public class CustomersController : ControllerBase
     // POST: api/Customers
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPost]
-    public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+    public ActionResult<Customer> PostCustomer(Customer customer)
     {
-        if (_context.Customers == null)
+        if (!_repository.GetAllCustomers().Any())
         {
-            return Problem("Entity set 'OrderContext.Customers'  is null.");
+            return NotFound();
         }
-       
-        _context.Customers.Add(customer);
-        await _context.SaveChangesAsync();
-
-
+        _repository.CreateCustomer(customer);
+        _repository.SaveChanges();
         return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
     }
 
 
     // DELETE: api/Customers/5
     [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteCustomer(int id)
+    public IActionResult DeleteCustomer(int id)
     {
-        if (_context.Customers == null)
+        if (!_repository.GetAllCustomers().Any())
         {
             return NotFound();
         }
-        var customer = await _context.Customers.FindAsync(id);
+        var customer=_repository.GetCustomerByID(id);
         if (customer == null)
         {
             return NotFound();
         }
-
-
-        _context.Customers.Remove(customer);
-        await _context.SaveChangesAsync();
-
-
+        _repository.DeleteCustomer(id);
+        _repository.SaveChanges();  
         return NoContent();
     }
 
 
-    private bool CustomerExists(int id)
+    // GET: api/Customers/GUID/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    [HttpGet("GUID/{id:guid}")]
+    public ActionResult<Customer> GetCustomerbyGUID(Guid id)
     {
-        return (_context.Customers?.Any(e => e.CustomerId == id)).GetValueOrDefault();
+        var customer = _repository.GetCustomerByGUID(id);
+        if (customer == null)
+        {
+            return NotFound();
+        }
+        return customer;
     }
+
+
+    // GET: api/Customers/SID/A123456789
+    // ^[A-Za-z][12]\d{8}$
+    [HttpGet("SID/{sid:regex(^[[A-Za-z]][[12]]\\d{{8}}$)}")]
+    public async Task<ActionResult<Customer>> GetCustomerSID(string sid)
+    {
+        var customer = _repository.GetCustomerBySecurityID(sid);
+        if (customer == null)
+        {
+            return NotFound();
+        }
+        return customer;
+    }
+
+
+    // GET: api/Customers/ListOrder/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    [HttpGet("ListOrder/{id:guid}")]
+    public ActionResult<IEnumerable<OrderDetailViewModel>> GetCustomerOrderbyGUID(Guid id)
+    {
+       
+        var order=_repository.GetCustomerOrderByGUID(id);  
+        if (order == null)
+        {
+            return NotFound();
+        }
+        else {
+            return Ok(order);
+        }
+    }
+
+
+    [HttpGet("sql/{companyname}")]
+    public IActionResult JsonBySQL(string companyname)
+    {
+        //using System.Data.SqlClient;
+        var cn = new SqlConnection(_config.GetConnectionString("DefaultConnection2"));
+        var cmd = cn.CreateCommand();//SqlCommand
+        cmd.CommandText ="Select C.CustomerId,C.CompanyName,O.OrderId,O.OrderDate,P.ProductId,P.ProductName,OP.Quantity,OP.UnitPrice,OP.Discount " +
+                            "From [dbo].[Customers] C join [dbo].[Orders] O "+
+                            "on C.CustomerId=O.CustomerId " +
+                            "join [dbo].[OrderProducts] OP " +
+                            "on O.OrderId=OP.OrderId " +
+                            "join [dbo].[Products] P " +
+                            "on P.ProductId=OP.ProductId " +
+                            "where C.CompanyName=@CompanyName";
+        cmd.Parameters.AddWithValue("@CompanyName",companyname);
+        cn.Open();
+        //using System.Data;
+        var dr = cmd.ExecuteReader();//SqlDataReader
+        //using System.Collections;
+        ArrayList list = new ArrayList();
+        while (dr.Read())
+        {
+            var data = new
+            {
+                CustomerId = dr["CustomerId"].ToString(),
+                CompanyName = dr["CompanyName"].ToString(),
+                OrderId = dr["OrderId"].ToString(),
+                OrderDate = Convert.ToDateTime(dr["OrderDate"]).ToString("d"),
+                ProductId = dr["ProductId"].ToString(),
+                ProductName = dr["ProductName"].ToString(),
+                UnitPrice=dr["UnitPrice"].ToString(),
+                Quantity =dr["Quantity"].ToString(),
+                Discount = dr["Discount"].ToString()
+            };
+            list.Add(data);
+        }
+        dr.Close();
+        cn.Close();
+        return Ok(list);          
+    }
+   
+    [HttpGet("{login:alpha}/{password:length(8,20)}/{sid:regex(^[[A-Za-z]][[12]]\\d{{8}}$)}")]
+    public ActionResult<string> Get(string login,string password,string sid)
+    {
+        return ($"您登入帳號是:{login},密碼是:{password},身份證:{sid}");
+    }
+
 }
