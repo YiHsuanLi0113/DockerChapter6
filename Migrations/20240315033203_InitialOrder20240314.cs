fde4deb6e0dc878142dac10b6e4d66bd1cf8922c
ysl19950113 <ysl19950113@gmail.com>
2024-03-15 11:33:50 +0800

    '6_1_Model_ManyToMany_移轉套用至資料庫'

---
+++ Migrations/20240315033203_InitialOrder20240314.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ManyToManyCodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialOrder20240314 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoginName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SecurityID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UnitPrice = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Freight = table.Column<float>(type: "real", nullable: true),
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderProducts",
+                columns: table => new
+                {
+                    OrderId = table.Column<short>(type: "smallint", nullable: false),
+                    ProductId = table.Column<short>(type: "smallint", nullable: false),
+                    UnitPrice = table.Column<float>(type: "real", nullable: false),
+                    Quantity = table.Column<short>(type: "smallint", nullable: false),
+                    Discount = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderProducts", x => new { x.OrderId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_OrderProducts_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "CompanyName", "Email", "LoginName", "SecurityID" },
+                values: new object[,]
+                {
+                    { 1, "Microsoft", "microsoft@test.com", "microsoft", "A123456789" },
+                    { 2, "Amazon", "amazon@test.com", "amazon", "A123456798" },
+                    { 3, "Google", "google@test.com", "google", "A123456789" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "ProductName", "UnitPrice" },
+                values: new object[,]
+                {
+                    { (short)1, "iPhone", 30000f },
+                    { (short)2, "iMac", 50000f },
+                    { (short)3, "iPad", 20000f }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "OrderId", "CustomerId", "Freight", "OrderDate" },
+                values: new object[,]
+                {
+                    { (short)1, 1, 80f, new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { (short)2, 2, 80f, new DateTime(2022, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { (short)3, 3, 80f, new DateTime(2022, 3, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { (short)4, 1, 80f, new DateTime(2022, 4, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "OrderProducts",
+                columns: new[] { "OrderId", "ProductId", "Discount", "Quantity", "UnitPrice" },
+                values: new object[,]
+                {
+                    { (short)1, (short)1, 0.1f, (short)1, 30000f },
+                    { (short)1, (short)3, 0.1f, (short)1, 50000f },
+                    { (short)2, (short)2, 0f, (short)1, 20000f },
+                    { (short)3, (short)1, 0f, (short)2, 29000f },
+                    { (short)4, (short)2, 0.15f, (short)1, 50000f },
+                    { (short)4, (short)3, 0.15f, (short)1, 20000f }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderProducts_ProductId",
+                table: "OrderProducts",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderProducts");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
